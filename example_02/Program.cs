4465ed4c1483cab5f33de1f36f1138eb6ed50cd0
WIN-JJH83PJTM7J\mkadmin <r.yakupov81@yandex.ru>
2022-08-30 17:39:35 +0300

    added  example_02

---
+++ example_02/Program.cs
@@ -0,0 +1,26 @@
+﻿// В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+
+// Метод для генерации значений элементов массива (с диапазоном от min до max)
+void RandomArrayElement(int[] position, int min, int max)
+{
+    Random rand = new Random();
+    int i = 0;
+    while(i<position.Length)
+    {
+        position[i] = rand.Next(min,max);
+        i++;
+    }
+}
+// Метод для вывода на экран элементов массива
+void PrintArray(int[] position)
+{
+    foreach(int element in position)
+        Console.Write($"{element} ");
+    Console.WriteLine();
+}
+
+Console.Write("Введите длинну массива:");
+int LengthArray = int.Parse(Console.ReadLine());
+int[] array = new int[LengthArray];
+
+RandomArrayElement(array, -200, 200);
