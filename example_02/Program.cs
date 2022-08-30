3b08c21fd02baa3123b6e82a2ba4aedddd85e9df
WIN-JJH83PJTM7J\mkadmin <r.yakupov81@yandex.ru>
2022-08-30 17:53:43 +0300

    added  example_02 correct

--- example_02/Program.cs
+++ example_02/Program.cs
@@ -18,9 +18,21 @@ void PrintArray(int[] position)
         Console.Write($"{element} ");
     Console.WriteLine();
 }
-
+// Метод подсчета элементов массива входящих в заданный диапазон
+int CountElementInRange(int[] mas)
+{
+    int i = 0;
+    foreach(int element in mas)// Можно сделать через цикл for
+        if((element <= 99) && (element >= 10))
+        {
+            i++;
+        };
+    return i;
+}
 Console.Write("Введите длинну массива:");
 int LengthArray = int.Parse(Console.ReadLine());
 int[] array = new int[LengthArray];
 
 RandomArrayElement(array, -200, 200);
+PrintArray(array);
+Console.WriteLine($"количество элементов массива в диапазоне [10,99] = {CountElementInRange(array)}");
