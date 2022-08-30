2778ae6be80fe56782b7a264fb9095b15a2d7069
WIN-JJH83PJTM7J\mkadmin <r.yakupov81@yandex.ru>
2022-08-30 15:03:26 +0300

    added first example_01

---
+++ example_01/Program.cs
@@ -0,0 +1,33 @@
+﻿// Определить, присутствует ли в заданном массиве, некоторое число
+
+bool FindNumberInArray(int[] mas, int number)
+{
+    int i;
+    bool result = false;
+    for (i = 0; i < mas.Length; i++)
+    {
+        if (mas[i] == number)
+        {
+            result = true;
+        }
+    }
+    return result;
+}
+int NumberEntry()// Функция ввода числа
+{
+    int Number;
+    while (!int.TryParse(Console.ReadLine(), out Number))//проверка что вводится число
+        Console.Write("Неверный ввод! \nВведите число n: ");
+    return Number;
+}
+Console.WriteLine("Введите число n, которое надо найти в массиве: ");
+int n = NumberEntry();
+int[] array = { 1, 2, 3, 4, 5, 2, 7, 8, 9 };
+if (FindNumberInArray(array, n) == true)
+{
+    Console.WriteLine($"Число {n} входит в массив");
+}
+else
+{
+    Console.WriteLine($"Число {n} не входит в массив");
+}
